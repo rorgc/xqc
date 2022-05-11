04137b1399b62f55f498817a2aeaef301f9e30cc
rorgc <105383253+rorgc@users.noreply.github.com>
2022-05-11 22:57:27 +0530

    Add files via upload

---
+++ MyWebAppp/src/main/java/com/rorgc/demo/MyWebApppApplication.java
@@ -0,0 +1,13 @@
+package com.rorgc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebApppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebApppApplication.class, args);
+	}
+
+}
---
+++ MyWebAppp/src/test/java/com/rorgc/demo/MyWebApppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rorgc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebApppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
